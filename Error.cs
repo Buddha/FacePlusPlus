e165a292e2c583e1891a4d846882fb582655d1eb
Vincent <376787823@qq.com>
2017-05-22 11:06:34 +0800

    Add files via upload

---
+++ Error.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacePlusPlus.Contract
+{
+    public class Error
+    {
+        public Error()
+        {
+
+        }
+
+        public int time_used { get; set; }
+
+        public int error_message { get; set; }
+
+        public string request_id { get; set; }
+    }
+}
---
+++ FaceSetResult.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacePlusPlus.Contract
+{
+    public class FaceSetResult
+    {
+        public FaceSetResult()
+        {
+
+        }
+
+        public string request_id { get; set; }
+
+        public string faceset_token { get; set; }
+
+        public string outer_id { get; set; }
+
+        public string display_name { get; set; }
+
+        public string user_data { get; set; }
+
+        public string tags { get; set; }
+
+        public int face_added { get; set; }
+
+        public int face_removed { get; set; }
+
+        public int face_count { get; set; }
+
+        public string[] face_tokens { get; set; }
+
+        public FaceSetCreateResultFailure[] failure_detail { get; set; }
+
+        public int time_used { get; set; }
+
+        public string error_message { get; set; }
+    }
+
+    public class FaceSetCreateResultFailure
+    {
+        public FaceSetCreateResultFailure()
+        {
+
+        }
+
+        public string face_token { get; set; }
+
+        public string reason { get; set; }
+    }
+}
